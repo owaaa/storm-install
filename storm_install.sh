f45e7dc2c3c7b1282efea77a1c70c027d5cbe3af
Andrew Aslinger <aaslinger@openwhere.com>
2013-11-27 11:46:29 -0500

    Update to install Oracle SDK

--- storm_install.sh
+++ storm_install.sh
@@ -52,6 +52,19 @@ EOF
     sudo chkconfig supervisord on
     sudo chmod 600 /etc/supervisord.conf
     
+    wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk-7u3-download-1501626.html;" http://download.oracle.com/otn-pub/java/jdk/7u3-b04/jdk-7u3-linux-x64.rpm
+    sudo rpm -ivh jdk-7u3-linux-x64.rpm
+    sudo /usr/sbin/alternatives --install /usr/bin/java java /usr/java/jdk1.7.0_03/bin/java 20000
+    sudo /usr/sbin/alternatives --set java /usr/java/jdk1.7.0_03/bin/java
+    
+    if grep --quiet JAVA_HOME /etc/profile; 
+    then 
+        echo JAVA_HOME already set;   
+    else  
+        sudo sh -c " echo 'export JAVA_HOME=/usr/java/jdk1.7.0_03/' >> /etc/profile" ;  
+    fi
+    source /etc/profile
+    
 }
 
 #########################################
